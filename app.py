0142b5f0ba6caa7aef351b06ac4e82d9a66c93b7
thaitranduc <dthai9323@gmail.com>
2023-10-26 20:44:20 +0700

    final code

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ LOG.setLevel(logging.INFO)
 
 def scale(payload):
     """Scales Payload"""
-    LOG.info("Scaling Payload: \n${payload}")
+    LOG.info("Scaling Payload: \n{payload}")
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
